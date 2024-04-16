6b25204ab2d36230f8d446a6d0ac905f7693e0c8
Sérgio Peres <102839657+sergioperess@users.noreply.github.com>
2024-04-16 15:29:02 -0300

    Create SubRegionJson.java

---
+++ SubRegionJson.java
@@ -0,0 +1,81 @@
+import java.security.KeyPair;
+
+public class SubRegionJson extends Region {
+
+    private Region mainRegion;
+    private byte[] encryptedData;
+    private String serializedData;
+
+    public SubRegionJson(Region mainRegion) {
+        this.mainRegion = mainRegion;
+    }
+
+    public Region getMainRegion() {
+        return mainRegion;
+    }
+
+    public byte[] getEncryptedData() {
+        return encryptedData;
+    }
+
+    public String getSerializedData() {
+        return serializedData;
+    }
+
+    public void encryptAndSerializeData(KeyPair keyPair) {
+        EncryptionThread encryptionThread = new EncryptionThread();
+        JsonSerializationThread jsonSerializationThread = new JsonSerializationThread();
+        encryptionThread.setRegion(this);
+        jsonSerializationThread.setRegion(this);
+        encryptionThread.start();
+        try {
+            encryptionThread.join(); // Aguardar a conclusão da criptografia
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        jsonSerializationThread.start();
+        try {
+            jsonSerializationThread.join(); // Aguardar a conclusão da serialização JSON
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private class EncryptionThread extends Thread {
+        private SubRegionJson subRegion;
+
+        public void setRegion(SubRegionJson subRegion) {
+            this.subRegion = subRegion;
+        }
+
+        @Override
+        public void run() {
+            try {
+                // Criptografar os dados da SubRegião
+                byte[] encryptedData = encryptRSA(subRegion.toString().getBytes(), keyPair.getPrivate());
+                subRegion.encryptedData = encryptedData;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private class JsonSerializationThread extends Thread {
+        private SubRegionJson subRegion;
+
+        public void setRegion(SubRegionJson subRegion) {
+            this.subRegion = subRegion;
+        }
+
+        @Override
+        public void run() {
+            try {
+                // Serializar a SubRegião para formato JSON
+                String json = subRegion.toJson();
+                subRegion.serializedData = json;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
